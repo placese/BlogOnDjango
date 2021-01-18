a7091ee2b21c932f75cdc302fd27c867b99bee79
placese <thegunter@yandex.ru>
2021-01-19 01:23:47 +0300

    create models, posts views

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from .models import Post
 
 # Create your views here.
 def posts_list(request):
-	n = ['Artem', 'Ksu']
-	return render(request, 'blog/index.html', context={'names': n})
+	posts = Post.objects.all()
+	return render(request, 'blog/index.html', context={'posts': posts})
+
+
+def post_detail(request, slug):
+	post = Post.objects.get(slug__iexact=slug)
+	return render(request, 'blog/post_detail.html', context={'post': post})
