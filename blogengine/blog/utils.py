60629782439fdf331da1eb89868494331b67f956
placese <thegunter@yandex.ru>
2021-01-21 23:36:37 +0300

    some updates

--- blogengine/blog/utils.py
+++ blogengine/blog/utils.py
@@ -10,3 +10,5 @@ class ObjectDetailMixin:
 	def get(self, request, slug):
 		obj = get_object_or_404(self.model, slug__iexact=slug)
 		return render(request, self.template, context={self.model.__name__.lower(): obj})
+
+
