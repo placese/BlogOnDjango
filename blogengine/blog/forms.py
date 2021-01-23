2f2ca3d247410c7b5a84816239062b874197b2f9
placese <thegunter@yandex.ru>
2021-01-23 23:51:11 +0300

    post form model + small refactor

--- blogengine/blog/forms.py
+++ blogengine/blog/forms.py
@@ -23,3 +23,24 @@ class TagForm(forms.ModelForm):
 		if Tag.objects.filter(slug__iexact=new_slug).count():
 			raise ValidationError(f'Slug must be unique. We have "{new_slug}" slug already.')
 		return new_slug
+
+
+class PostForm(forms.ModelForm):
+
+	class Meta:
+		model = Post
+		fields = ['title', 'slug', 'body', 'tags']
+
+		widgets = {
+			'title': forms.TextInput(attrs={'class': 'form-control'}),
+			'slug': forms.TextInput(attrs={'class': 'form-control'}),
+			'body': forms.Textarea(attrs={'class': 'form-control'}),
+			'tags': forms.SelectMultiple(attrs={'class': 'form-select'})
+		}
+
+	def clean_slug(self):
+		new_slug = self.cleaned_data['slug'].lower()
+
+		if new_slug == 'create':
+			raise ValidationError('Slug may not be "create"')
+		return new_slug
--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View
 
 from .models import Post, Tag
-from .utils import ObjectDetailMixin
-from .forms import TagForm
+from .utils import *
+from .forms import TagForm, PostForm
 
-# Create your views here.
+
 def posts_list(request):
 	posts = Post.objects.all()
 	return render(request, 'blog/index.html', context={'posts': posts})
@@ -16,25 +16,21 @@ class PostDetail(ObjectDetailMixin, View):
 	template = 'blog/post_detail.html'
 
 
-class TagDetail(ObjectDetailMixin, View):
-	model = Tag
-	template = 'blog/tag_detail.html'
-	
-
-class TagCreate(View):
-	def get(self, request):
-		form = TagForm
-		return render(request, 'blog/tag_create.html', context={'form': form})
-
-	def post(self, request):
-		bound_form = TagForm(request.POST)
-
-		if bound_form.is_valid():
-			new_tag = bound_form.save()
-			return redirect(new_tag)
-		return render(request, 'blog/tag_create.html', context={'form': bound_form})
+class PostCreate(ObjectCreateMixin, View):
+	model_form = PostForm
+	template = 'blog/post_create_form.html'
 
 
 def tags_list(request):
 	tags = Tag.objects.all()
 	return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+
+class TagDetail(ObjectDetailMixin, View):
+	model = Tag
+	template = 'blog/tag_detail.html'
+	
+
+class TagCreate(ObjectCreateMixin, View):
+	model_form = TagForm
+	template = 'blog/tag_create.html'
