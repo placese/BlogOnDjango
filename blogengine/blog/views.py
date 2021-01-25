007227c2b746de2751dd914a33182899f10fdb45
placese <thegunter@yandex.ru>
2021-01-25 23:04:25 +0300

    createed delete form to tags and posts

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View
+from django.urls import reverse
 
 from .models import Post, Tag
 from .utils import *
@@ -26,6 +27,12 @@ class PostUpdate(ObjectUpdateMixin, View):
 	model_form = PostForm
 	template = 'blog/post_update_form.html'
 
+class PostDelete(ObjectDeleteMixin, View):
+	model = Post
+	template = 'blog/post_delete_form.html'
+	redirect_url = 'posts_list_url'
+
+
 
 def tags_list(request):
 	tags = Tag.objects.all()
@@ -46,3 +53,9 @@ class TagUpdate(ObjectUpdateMixin, View):
 	model = Tag
 	model_form = TagForm
 	template = 'blog/tag_update_form.html'
+
+
+class TagDelete(ObjectDeleteMixin, View):
+	model = Tag
+	template = 'blog/tag_delete_form.html'
+	redirect_url = 'tags_list_url'
